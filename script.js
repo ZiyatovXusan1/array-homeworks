a1cc2a24645c63e19719a8c2f8b6bacdf1e0b831
ZiyatovXusan1 <xusan0083@gmail.com>
2025-09-02 16:58:23 +0500

    New project files

---
+++ script.js
@@ -0,0 +1,10 @@
+// ! 1-masala
+// function getLevel2(n) {
+//     let natija = [];
+//     for (let i = 1; i <= n; i++) {
+//         result.push(2 ** i);
+//     }
+//     return natija;
+// }
+
+// console.log(getLevel2(6));
